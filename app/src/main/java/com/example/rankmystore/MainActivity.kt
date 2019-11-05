b10329005126adb1be886d8a3dd48e90e962e781
zhuang@umd.edu <jameshuang@huangzhiyudeMacBook-Pro.local>
2019-11-05 11:28:23 -0500

    First commit

---
+++ app/src/main/java/com/example/rankmystore/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.rankmystore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun processLogin(user_name:String, password:String){
+        val intent = Intent(this, SuccessLogin::class.java)
+        intent.putExtra("user", user_name)
+        intent.putExtra("pass",password)
+        Toast.makeText(applicationContext, "Logging you in...!",Toast.LENGTH_SHORT).show()
+        startActivity(intent)
+    }
+    /*
+
+    var uName = etUName.text.toString()
+        var uPwd = etUPass.text.toString()
+        var pwd = uName + "@123"
+     */
+    fun checkLogin(view: View){
+       // var uName =
+    }
+}
+
+
---
+++ app/src/main/java/com/example/rankmystore/SuccessLogin.kt
@@ -0,0 +1,20 @@
+package com.example.rankmystore
+
+import android.content.Intent
+import android.view.View
+import android.widget.AbsListView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SuccessLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout)
+
+    }
+}
+
